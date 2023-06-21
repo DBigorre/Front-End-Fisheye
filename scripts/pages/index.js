9983bfb6d07a883ead277ba76f73f6971c2d33e2
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 10:42:17 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,7 @@
 async function getPhotographers() {
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  fetch("../../data.json")
+  fetch("../../data/data.json")
     .then(res=>console.log(res))
 }
 
