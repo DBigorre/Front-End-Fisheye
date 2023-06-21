7859db205391a487700cc5fd02e16b3857c3bbef
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 13:29:59 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,7 +3,7 @@ async function getPhotographers() {
     const response = await fetch("../../data/photographers.json");
     const data = await response.json();
     if (data && Array.isArray(data.photographers)) {
-      return data.photographers;
+      return data;
     } else {
       console.error("Les données des photographes ne sont pas au format attendu.");
     }
