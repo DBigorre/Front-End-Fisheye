fd9e28402c4d7c0646d46d0e931cd042856a73a2
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 13:42:06 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -12,21 +12,20 @@ async function getPhotographers() {
   }
 }
 
-/*async function displayData(photographers) {
+async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
   const photographerNames = photographersSection.map((photographer) => photographer.name);
-  console.log(photographerNames)
 
   photographers.forEach((photographer) => {
       const photographerModel = photographerTemplate(photographer);
       const userCardDOM = photographerModel.getUserCardDOM();
       photographersSection.appendChild(userCardDOM);
   });
-}*/
+}
 
 async function init() {
   // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
+  const photographers = await getPhotographers();
   displayData(photographers);
 }
 
