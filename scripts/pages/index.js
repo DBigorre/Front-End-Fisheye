4ab97a896f0d281ce94092988b22582d45fea30e
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 13:27:26 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,7 +2,7 @@ async function getPhotographers() {
   try {
     const response = await fetch("../../data/photographers.json");
     const data = await response.json();
-    console.log(data);
+    
   } catch (error) {
     console.error("Erreur:", error);
   }
