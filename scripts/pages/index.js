efb1f7e33f8f69756848900220da721229e45dce
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 14:08:42 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -15,8 +15,8 @@ async function getPhotographers() {
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
-  const photographerNames = photographers.map((photographer) => photographer.name);
   console.log(photographers)
+  const photographerNames = photographers.map((photographer) => photographer.name);
   photographers.forEach((photographer) => {
       const photographerModel = photographerTemplate(photographer);
       const userCardDOM = photographerModel.getUserCardDOM();
