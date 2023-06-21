475cf586a6392b572a2aa0c2aceeda6a612f15d6
desiree bigorre <bigorredesiree@gmail.com>
2023-06-21 11:32:41 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,18 +1,17 @@
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
   try {
     const response = await fetch("../../data/photographers.json");
     const data = await response.json();
     console.log(JSON.stringify(data));
   } catch (error) {
-    console.error("Une erreur s'est produite lors de la récupération des photographes :", error);
+    console.error("Erreur:", error);
   }
 }
 
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
+  const photographerNames = photographersSection.map((photographer) => photographer.name);
 
   photographers.forEach((photographer) => {
       const photographerModel = photographerTemplate(photographer);
