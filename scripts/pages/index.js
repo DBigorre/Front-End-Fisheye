fce4757e7faba3d4d0b26dc226d830cf2f452dcf
desiree bigorre <bigorredesiree@gmail.com>
2023-06-26 11:43:33 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -30,9 +30,9 @@ async function displayData(data) {
   });
   for (let photographer of arr) {
     let photographerData = new Photographer(photographer.value.name, photographer.value.id, photographer.value.city, photographer.value.country, photographer.value.tagline, photographer.value.portrait);
-    return photographerData
+    return photographerData;
   };
-  console.log(photographerData);
+  console.log(Photographer);
 }
 
 /*console.log(data.map(photographer))
