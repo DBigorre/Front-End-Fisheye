55ecb4949141cbc7290a6cf5d42a4f327fa5d24b
desiree bigorre <bigorredesiree@gmail.com>
2023-06-27 09:51:11 -0400

    test prod api

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -28,17 +28,19 @@ async function displayData(data) {
   const arr = Object.entries(data.photographers).map(([key, value]) => {
     return { key, value };
   });
-  photographersArray = [];
+  let photographersArray = [];
   for (let photographer of arr) {
     let photographerData = new Photographer(photographer.value.name, photographer.value.id, photographer.value.city, photographer.value.country, photographer.value.tagline, photographer.value.portrait);
     photographersArray.push(photographerData);
   };
+  return photographersArray;
 }
 
 async function init() {
   // Récupère les datas des photographes
   const photographers = await getPhotographers();
-  displayData(photographers);
+  const photographersArray = await displayData(photographers);
+  //displayData(photographers);
   console.log(photographersArray)
 }
 
